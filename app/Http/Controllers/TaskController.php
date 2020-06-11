ace3be902b8e3c4a5f4bedba7f2fc1078d11505e
EC2 Default User <ec2-user@ip-172-31-7-193.ap-northeast-1.compute.internal>
2020-06-11 01:32:28 +0000

    add status

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -10,10 +11,15 @@ class TaskController extends Controller
     public function index(int $id)
     {
         $folders = Folder::all();
+        
+        $current_folder = Folder::find($id);
+        
+        $tasks = $current_folder->tasks()->get();;
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
