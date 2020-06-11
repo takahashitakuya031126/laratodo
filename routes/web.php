6b875caadc214892f1228fb05c09e70966874d9b
EC2 Default User <ec2-user@ip-172-31-7-193.ap-northeast-1.compute.internal>
2020-06-11 01:57:16 +0000

    add create-form

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@
 */
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
